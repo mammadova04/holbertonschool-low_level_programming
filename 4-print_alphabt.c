4bf420cc8618e4bb2fdae7e017fd988084e2fb98
mammadova04 <nmammadova04@gmail.com>
2024-02-20 02:50:14 -0800

    Write a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
