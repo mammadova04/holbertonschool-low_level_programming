0b7e05e9f5d354df7da9e005d19028cec2bb551c
mammadova04 <nmammadova04@gmail.com>
2024-02-27 11:21:59 +0000

    Write a function that checks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isdigit - checks
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int _isdigit(int c)
+{
+	if (c <= 57 && c >= 48)
+	{
+		return (1);
+	}
+	
+	return (0);
+}
