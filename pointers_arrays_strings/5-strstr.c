ca4bbcb8b33fc8e9052615e1bd485a2fc126bfd9
mammadova04 <nmammadova04@gmail.com>
2024-03-04 13:51:38 +0000

    Write a function that locates a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * _strstr - func
+ * @haystack: char
+ * @needle: char
+ *
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n)
+		{
+			h++;
+			n++;
+		}
+		if (!(*n))
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
