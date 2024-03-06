799ddfd8c095a244cd0efdee81ce214ad6f7eafc
mammadova04 <nmammadova04@gmail.com>
2024-03-06 13:37:17 +0000

    Write a function that returns the natural square root of a number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,13 +7,19 @@
  */
 int _helper(int x, int y)
 {
-	if (y % x != 0)
-		return (0);
-	else if (x * 2 < y)
+	if (x > y / 2)
 		return (1);
+	else if (!(y % x))
+		return (0);
 	else
 		return (_helper(x + 1, y));
 }
+/**
+ * is_prime_number - checks whether number is prime
+ * @n: checker
+ *
+ * Return: int
+ */
 int is_prime_number(int n)
 {
 	if (n < 0 || n == 1)
