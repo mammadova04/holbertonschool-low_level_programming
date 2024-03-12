8f996d307ea19adf34c2004e39476a0207f98b76
mammadova04 <nmammadova04@gmail.com>
2024-03-12 11:18:36 +0000

    fixed memory error

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int *) * (height + 1));
+	grid = malloc(sizeof(int *) * (height));
 	if (grid == NULL)
 	{
 		free(grid);
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc((width + 1) * sizeof(int));
+		grid[i] = malloc((width) * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			while (i >= 0)
