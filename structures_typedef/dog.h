db15d39e3028482516ae3ef73b25c52776f7aa9f
mammadova04 <nmammadova04@gmail.com>
2024-03-13 13:03:29 +0000

    a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ * Description: dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
