a153fa1914c2487261031263c3e03f7b8b2cbcc8
mammadova04 <nmammadova04@gmail.com>
2024-03-14 11:39:17 +0000

    fixed error

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,46 +3,51 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+
 /**
- * new_dog - creates new dog
- * @name: dog.name
- * @age: dog.age
- * @owner: dog.owner
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  *
- * Return: dog_t
+ * Return: pointer to the new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, len_name = 0, len_owner = 0;
-
-	dog_t *dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
-		return (NULL);
+	dog_t *dog;
 
 	while (*(name + len_name))
 		len_name++;
 	while (*(owner + len_owner))
 		len_owner++;
-	
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 	(*dog).name = malloc(sizeof(char) * (len_name + 1));
 	if ((*dog).name == NULL)
 	{
-		free((*dog).name), free(dog);
+		free(dog);
 		return (NULL);
 	}
+
+	for (i = 0; i < len_name; i++)
+		(*dog).name[i] = name[i];
+	(*dog).name[len_name] = '\0';  /* Null-terminate the string */
 
 	(*dog).age = age;
 
 	(*dog).owner = malloc(sizeof(char) * (len_owner + 1));
 	if ((*dog).owner == NULL)
 	{
-		free((*dog).owner), free((*dog).name), free(dog);
+		free((*dog).name);
+		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < len_name; i++)
-		*((*dog).name + i) = *(name + i);
+
 	for (i = 0; i < len_owner; i++)
-                *((*dog).owner + i) = *(owner + i);
+		(*dog).owner[i] = owner[i];
+	(*dog).owner[len_owner] = '\0';  /* Null-terminate the string */
+
 	return (dog);
 }
