cbbeb6f847673f1282e1b0f3ee57a3c39e377885
mammadova04 <nmammadova04@gmail.com>
2024-03-21 11:16:27 +0000

    added file

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stddef.h>
+/**
+ * get_op_func - get op func
+ * @s: s
+ * Return: int
+ */
+int (*get_op_func(char *s))(int n, int m)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_sub},
+		{"/", op_sub},
+		{"%", op_sub},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i < (int)(sizeof(ops) / sizeof(op_t)))
+	{
+		if (*s == *ops[i].s)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
