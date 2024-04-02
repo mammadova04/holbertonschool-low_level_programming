f3c521b144e1355be00e5d72e78d39a5e36a0b9b
mammadova04 <nmammadova04@gmail.com>
2024-04-02 13:36:06 +0000

    removed strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,14 +11,14 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
+        list_t *temp;
 
-	temp = malloc(sizeof(list_t));
-	if (!temp)
-		return (NULL);
-	temp->str = strdup(str);
-	temp->len = strlen(str);
-	temp->next = *head;
-	*head = temp;
-	return (temp);
+        temp = malloc(sizeof(list_t));
+        if (!temp)
+                return (NULL);
+        temp->str = strdup(str);
+        temp->len = strlen(str);
+        temp->next = *head;
+        *head = temp;
+        return (temp);
 }
