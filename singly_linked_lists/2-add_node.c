2374ca12bdbe2bc6222ca6acbe98ccad7a2bd378
mammadova04 <nmammadova04@gmail.com>
2024-04-02 13:44:16 +0000

    fixed betty error

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,23 +11,24 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *temp;
-    int len = 0;
+	list_t *temp;
+	int len = 0;
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    temp = malloc(sizeof(list_t));
-    if (!temp)
-        return (NULL);
-    temp->str = strdup(str);
-    if (!temp->str) {
-        free(temp);
-        return (NULL);
-    }
-    temp->len = len;
-    temp->next = *head;
-    *head = temp;
-    return (temp);
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+		return (NULL);
+	temp->str = strdup(str);
+	if (!temp->str)
+	{
+		free(temp);
+	return (NULL);
+	}
+	temp->len = len;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
 
